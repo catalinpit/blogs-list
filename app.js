b5f015f637971f6f90ed4595a02f65315b1df7b7
catalinpit <catalinpit@gmail.com>
2022-01-20 17:30:20 +0200

    Refactoring

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+require('express-async-errors');
+const app = express();
+const cors = require('cors');
 const blogRouter = require('./src/controllers/blogs');
 require('./src/database/db');
-
-const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
